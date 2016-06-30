18981d322f4ddcab2e80d82e9fd8262e918b0770
Abhijit Mitkar <abey350@gmail.com>
2016-06-30 20:09:32 +0530

    Fab popup options

--- app/src/main/java/com/abhijitm/wardrobe/ActMain.java
+++ app/src/main/java/com/abhijitm/wardrobe/ActMain.java
@@ -1,10 +1,13 @@
 package com.abhijitm.wardrobe;
 
+import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,11 +19,14 @@ public class ActMain extends AppCompatActivity {
     private ViewPager viewPagerTop;
     private ViewPager viewPagerBottom;
     private FloatingActionButton fab;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        context = this;
 
         // initialize views
         // toolbar
@@ -31,12 +37,7 @@ public class ActMain extends AppCompatActivity {
         viewPagerBottom = (ViewPager) findViewById(R.id.actMain_viewpagerBottom);
         // fab
         fab = (FloatingActionButton) findViewById(R.id.actMain_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
+        fab.setOnClickListener(onClickListener);
     }
 
     @Override
@@ -56,4 +57,27 @@ public class ActMain extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            PopupMenu popupMenu = new PopupMenu(context, view, GravityCompat.END);
+            popupMenu.inflate(R.menu.menu_fab_options);
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.menuFab_camera:
+                            // TODO
+                            return true;
+                        case R.id.menuFab_picker:
+                            // TODO
+                            return true;
+                    }
+                    return false;
+                }
+            });
+            popupMenu.show();
+        }
+    };
 }
