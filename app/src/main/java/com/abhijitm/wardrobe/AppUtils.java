2129b9bd6c5075d5cd940bc3a1a985478bdabf4b
Abhijit Mitkar <abey350@gmail.com>
2016-06-30 19:29:44 +0530

    app utils

---
+++ app/src/main/java/com/abhijitm/wardrobe/AppUtils.java
@@ -0,0 +1,29 @@
+package com.abhijitm.wardrobe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Abhijit on 30-06-2016.
+ */
+public class AppUtils {
+
+    /**
+     * Set Toolbar as ActionBar and set various properties
+     *
+     * @param appCompatActivity Activity instance (this)
+     * @param toolbarId Toolbar resource id (R.id.act_name_toolbar)
+     * @param title Title to be displayed on Toolbar. Pass null to keep default title from manifest
+     * @param enableBack To enable back caret button or not
+     */
+    public static void setUpToolbar(AppCompatActivity appCompatActivity, int toolbarId, String title, boolean enableBack) {
+        Toolbar toolbar = (Toolbar) appCompatActivity.findViewById(toolbarId);
+        appCompatActivity.setSupportActionBar(toolbar);
+        if (appCompatActivity.getSupportActionBar() != null) {
+            appCompatActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(enableBack);
+            if (title != null) {
+                appCompatActivity.getSupportActionBar().setTitle(title);
+            }
+        }
+    }
+}
